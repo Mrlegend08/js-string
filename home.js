893359db44ba8eefa0b6f35220040de7b93a09b6
Mrlegend08 <muslimdasturchi@gmail.com>
2023-01-09 18:08:19 +0500

    done

---
+++ home.js
@@ -0,0 +1,94 @@
+// 1
+// let helloName = "Asror"
+// console.log("hello" + " " + helloName+"!");
+
+
+// 2
+// function makeAbby (a , b){
+//     return a + b + b +a;
+// }
+
+// let abby = makeAbby("hi" , "bye");
+// console.log(abby);
+
+
+
+// 3
+// function makeTags(a, b){
+//     return `<${a}>${b}<${a}>`
+// }
+// console.log(makeTags("i" , "Yuri"))
+
+
+// 4
+// function makeOutWord(a , b){
+//     return `${a.slice(0 , 2)}${b}${a.slice(2, a.length)}`;
+// }
+// console.log(makeOutWord("<<>>" , "Yay")); 
+
+
+// 5
+// function extraEnd(a){
+//     return a.slice(-2)+a.slice(-2)+a.slice(-2);
+// }
+
+// console.log(extraEnd("hello"))
+
+
+// 6
+// function firstTwo(a) {
+//     return a.slice(0, 2) + a.slice(0, 2) + a.slice(0, 2);
+// }
+// console.log(firstTwo("hello"))
+
+
+
+// 7
+// function firsthalf(a){
+//     return a.slice(0, a.length/2)
+// }
+// console.log(firsthalf("helloThere"))
+
+
+// 8
+// var yourString = "java";
+// var result = yourString.substring(1, yourString.length-1);
+// console.log(result);
+
+
+
+// 9
+// function comboString(a, b){
+//     return b+a+b;
+// }
+// console.log(comboString("hello" , "b"))
+
+
+
+// 10
+// function nonStart(a, b) {
+//     return a.substring(1)+b.substring(1);
+// }
+// console.log(nonStart("hello", "java"))
+
+// 11 
+// function lefStart(a) {
+//     return a.substring(2) + a.slice(0 , 2);
+// }
+// console.log(lefStart("hi"))
+
+
+// 12
+// function lefStart(a) {
+//     let c = "";
+//     let d = "";
+//     for(let i = 0; i < a.length; i++){
+//         if(i < 2){
+//             c+=a[i];
+//         }else if(i >= 2){
+//             d+=a[i];
+//         }
+//     }
+//     return d + c;
+// }
+// console.log(lefStart("hi"))
---
+++ js/app.js
@@ -0,0 +1,31 @@
+// var yosh =  prompt("Tug'lgan yilingizni kiriting: ")
+// var ohiri = yosh.slice(2)
+// var asr = yosh.slice(0, 2)
+// if (ohiri == 00) {
+//     console.log("siz", asr, "asirda tugulgansiz")
+// } else {
+//     console.log(1 + +asr, ` asrda tugulgansiz`)
+// }
+
+
+
+
+// let ball = 94
+// if(ball >= 90 && ball <=100){
+//     console.log("Grant")
+// }else if(ball >= 70 && ball <=89){
+//     console.log("Kontrakt")
+// }else if(ball >= 50 && ball <=69){
+//     console.log("super kontrakt")
+// }else if(ball < 50){
+//     console.log("fail")
+// }
+
+
+// let yil = 2022
+
+// if ((yil % 4 === 0 &&  yil%100 !== 0) || yil % 400 === 0){
+//     console.log("Kabisa");
+// }else{
+//     console.log("kabisa emas");
+// }
